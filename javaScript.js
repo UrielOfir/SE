03bd9ef0661df491704021ec95b0d7ba33558ef3
uriel ofir <urielofir@gmail.com>
2018-09-08 19:48:21 +0300

    computer player

--- javaScript.js
+++ javaScript.js
@@ -4,9 +4,12 @@ let hand1={elem:"", player:player1, otherPlayer:player2, fingers:1, choose:false
 let hand2={elem:"", player:player1, otherPlayer:player2, fingers:1, choose:false};
 let hand3={elem:"", player:player2, otherPlayer:player1, fingers:1, choose:false};
 let hand4={elem:"", player:player2, otherPlayer:player1, fingers:1, choose:false};
+let handsMap=[hand1,hand2,hand3,hand4];
 let handWasChose=false;
 let fingersUp=0;
 let lastHand;
+let computer=false;
+
 
 announcement=document.getElementById("announcement");
 annText=document.getElementById("annText")
@@ -26,7 +29,12 @@ function show(elem){
 btnOK.addEventListener("click", function(event){
 	player1.name=document.getElementById("player1").value;
 	player2.name=document.getElementById("player2").value;
+	if(player1.name==""||player2.name==""){
+		alert("חובה להכניס שמות שני השחקנים")
+	}
+	else{
 	hide(announcement);
+	}
 })
 
 btnInst.addEventListener("click", function(event){
@@ -40,16 +48,48 @@ btnBack.addEventListener("click", function(event){
 })
 	
 
+comPlayer.addEventListener("click", function(event){
+	player1.name=document.getElementById("player1").value;
+	player2.name="המחשב";
+	computer=true;
+	if(player1.name==""||player2.name==""){
+		alert("חובה להכניס שם שחקן")
+	}
+	else{
+	hide(announcement);
+	}
+})
+	
 hand1.elem=document.getElementById("1");
 hand2.elem=document.getElementById("2");
 hand3.elem=document.getElementById("3");
 hand4.elem=document.getElementById("4");
 
+makeClick();
+function makeClick(){
+	hand1.elem.onclick=function(e){clicked(hand1)};
+	hand2.elem.onclick=function(e){clicked(hand2)};
+	hand3.elem.onclick=function(e){clicked(hand3)};
+	hand4.elem.onclick=function(e){clicked(hand4)};
+}
 
-hand1.elem.addEventListener("click", function(event){clicked(hand1)});
-hand2.elem.addEventListener("click", function(event){clicked(hand2)});
-hand3.elem.addEventListener("click", function(event){clicked(hand3)});
-hand4.elem.addEventListener("click", function(event){clicked(hand4)});
+function stopClick(){
+	hand1.elem.onclick=null;
+	hand2.elem.onclick=null;
+	hand3.elem.onclick=null;
+	hand4.elem.onclick=null;
+}
+
+function playRandom(){
+	stopClick();
+	if(hand3.choose||hand4.choose){
+	setTimeout(function(event){clicked(handsMap[Math.floor(Math.random()*4)])},1000);
+	}
+	else{
+	setTimeout(function(event){clicked(handsMap[Math.floor(Math.random()*2+2)])},1000);
+	}
+}
+
 
 function clicked(hand){
 	if (hand.player.turn&&hand.choose==false&&handWasChose==false&&hand.fingers!=0)
@@ -60,15 +100,36 @@ function clicked(hand){
 		fingersUp=hand.fingers;
 		hand.elem.style.filter="blur(5px)";
 		lastHand=hand;
+		if (computer&&player2.turn==true){
+				playRandom();
+			}
 	}
 	
 	else if (hand.player.turn&&hand.choose==false&&handWasChose&&hand.fingers==0)
 	{
-		if(lastHand.fingers%2==0)
+		if(lastHand.fingers%2==0){
 			lastHand.fingers=hand.fingers=lastHand.fingers/2;
 			hand.elem.src=hand.elem.src.slice(0,-5)+hand.fingers+".jpg";
 			lastHand.elem.src=lastHand.elem.src.slice(0,-5)+lastHand.fingers+".jpg";
 			lastHand.elem.style.filter="blur(0px)";
+			hand.player.turn=false;
+			hand.otherPlayer.turn=true;
+			if (computer&&player2.turn==true){
+				playRandom();
+			}
+			if(computer&&player1.turn==true){
+				makeClick();
+			}
+		}
+		else{
+			if (computer&&player2.turn==true){
+				playRandom();
+			}
+			if(computer&&player1.turn==true){
+				makeClick();
+			}
+		}
+			
 	}
 
 	else if(hand.player.turn==false&&handWasChose)
@@ -85,9 +146,19 @@ function clicked(hand){
 		hand.elem.src=hand.elem.src.slice(0,-5)+hand.fingers+".jpg";
 		lastHand.elem.style.filter="blur(0px)";
 		lastHand.choose=false;
-		chek();	
+		chek();
+		if (computer&&player2.turn==true){
+			playRandom();
+		}
+		if(computer&&player1.turn==true){
+				makeClick();
+			}		
 	}
-
+	else if(true){		
+			if (computer&&player2.turn==true){
+				playRandom();
+		}
+	}
 }
 
 function chek(){
